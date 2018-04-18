12ed6281acd9f5314d8d7f9cdfca4b9afd2b393f
Tomokazu Higuchi <higuchi@leokikaku.com>
2018-04-18 13:30:11 +0900

    no message

---
+++ slackbot_settings.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+# botアカウントのトークンを指定
+API_TOKEN = "xoxb-349187662407-Hs8eyybgITSgeB4ssZaMayCV"
+
+# このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
+DEFAULT_REPLY = "何言ってんだこいつ"
+
+# プラグインスクリプトを置いてあるサブディレクトリ名のリスト
+PLUGINS = ['plugins']
